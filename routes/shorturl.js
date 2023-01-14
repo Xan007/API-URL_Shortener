b65b4670c02b6899526a0bbc638bb93cf881d305
Sander Sierra <sierrasander29@gmail.com>
2023-01-13 23:52:53 -0500

    Añade modelo url y los endpoints

--- routes/shorturl.js
+++ routes/shorturl.js
@@ -1,15 +1,17 @@
 import { Router } from "express";
-import { promises as dnsPromises } from "node:dns";
+import { customAlphabet } from "nanoid";
+
+import urlModel from "../models/url.js";
+
+const httpRegex = /^(http(s)?:\/\/[a-zA-Z0-9\-_]+\.[a-zA-Z]+(.)+)+$/;
+
+const alphabet = "0123456789aAeEiIoOuU";
+const nanoid = customAlphabet(alphabet, 9);
 
 const router = Router();
 
 function isValidHttpUrl(string) {
-  try {
-    const newUrl = new URL(string);
-    return newUrl.protocol === "http:" || newUrl.protocol === "https:";
-  } catch (err) {
-    return false;
-  }
+  return httpRegex.test(string);
 }
 
 router.post("/", async (req, res) => {
@@ -20,16 +22,30 @@ router.post("/", async (req, res) => {
       error: "invalid url",
     });
 
-  res.send({
+  const newUrl = new urlModel({
     original_url: url,
-    short_url: "",
+    short_url: nanoid(),
   });
+
+  try {
+    await newUrl.save();
+
+    return res.send(newUrl);
+  } catch (err) {
+    if (err.code === 11000 && err.name === "MongoServerError")
+      return res.send(await urlModel.findOne({original_url: url}))
+    return res.status(400).send(err);
+  }
 });
 
-router.get("/:shortURL", (req, res) => {
+router.get("/:shortURL", async (req, res) => {
   const { shortURL } = req.params;
 
-  res.redirect();
+  const urlDoc = await urlModel.findOne({ short_url: shortURL });
+
+  if (!urlDoc) return res.status(404).send("No original_url for short_url");
+  
+  return res.redirect(urlDoc.original_url);
 });
 
 export default router;
