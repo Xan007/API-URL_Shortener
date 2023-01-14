cae18e6b1bad4f56d60f276b7465b638c154678a
Sander Sierra <sierrasander29@gmail.com>
2023-01-13 23:53:04 -0500

    Añade inicializacion de base de datos

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ dotenv.config();
 import router from "./routes/index.js";
 import bodyParser from "body-parser";
 
+import "./db.js"
+
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true}))
+app.use(express.json())
 app.use("/public", express.static(`${process.cwd()}/public`));
 
 app.use("/api", router);
